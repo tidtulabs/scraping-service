2fb2945ab741b3f09af1cd3f9b81f82a0c7c87f9
ngtuonghy <ngtuonghy@gmail.com>
2025-01-02 00:04:14 +0700

    add t

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,9 @@ import "dotenv/config";
 import cors from "cors";
 import routes from "./routes";
 import errorHandler from "@middlewares/error-handler";
-import ServerlessHttp from "serverless-http";
 import { redis } from "@libs/redis";
 import { logger } from "@libs/winston";
+const serverless = require('serverless-http');
 
 const app: Express = express();
 //const port = process.env.PORT || 3000;
@@ -33,20 +33,28 @@ app.use(errorHandler);
 //		});
 //	logger.info(`Server is running on port ${port}`);
 //});
+//
 
-const handler = ServerlessHttp(app);
-module.exports.handler = async (event: any, context: any) => {
-	redis
-		.connect()
-		.then(() => {
-			logger.info("Connected to Redis");
-		})
-		.catch((err) => {
-			logger.error(err.message);
-		});
-	// you can do other things here
-	const result = await handler(event, context);
-	// and here
-	return result;
-};
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+module.exports.handler = serverless(app);
+
+
+//const handler = ServerlessHttp(app);
+//module.exports.handler = async (event: any, context: any) => {
+//	redis
+//		.connect()
+//		.then(() => {
+//			logger.info("Connected to Redis");
+//		})
+//		.catch((err) => {
+//			logger.error(err.message);
+//		});
+//	// you can do other things here
+//	const result = await handler(event, context);
+//	// and here
+//	return result;
+//};
 //export default app;
