c25c45cd9aa7bd860d47735f7aa69056204f6aba
ngtuonghy <ngtuonghy@gmail.com>
2025-01-02 11:36:03 +0700

    a8

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
 import serverless from "serverless-http";
 import app from "./app";
-module.exports.handler = serverless(app);
+import { redis } from "@libs/redis";
+import { logger } from "@libs/winston";
+
+const handler = serverless(app);
+module.exports.handler = async (event: any, context: any) => {
+	redis
+		.connect()
+		.then(() => {
+			logger.info("Connected to Redis");
+		})
+		.catch((err) => {
+			logger.error(err.message);
+		});
+	// you can do other things here
+	const result = await handler(event, context);
+	// and here
+	return result;
+};
+
